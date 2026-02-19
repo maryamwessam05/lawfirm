fd8aa8e8469d1086d6a6f8fbe3e2fbe7b8b8821d
maryamwessam05 <23-101015@students.eui.edu.eg>
2026-02-20 01:50:54 +0200

    herosection

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import "./home.css";
 import Navbar from '../components/navbar/navbar';
+import heroBg from "../assets/heroimg.png"
 import { useState, useEffect } from "react";
+import Yellowbtn from '../components/yelowbtn/yellowbtn';
+import call from "../assets/call.svg"
 
 
 const Home = () => {
@@ -23,6 +26,24 @@ const Home = () => {
     return ( 
         <>
             <Navbar toggleTheme={() => setDark(!dark)} />
+            
+            <section className="herosection" style={{ backgroundImage: `url(${heroBg})` }}>
+                <div className="herotxt">
+                    <h1>Trusted Legal Excellence</h1>
+                    <p>With over 25 years of experience, we provide comprehensive legal solutions for individuals and businesses. Our commitment to integrity and client success sets us apart.</p>
+                    <div className="btns">
+                        <Yellowbtn text="Schedule Consultation" />
+                        <button className='serv'>Our Services</button>
+                    </div>
+                    <div className="call">
+                        <img src={call} alt="call" />
+                        <div className="calltxt">
+                            <span>Call us anytime</span>
+                            <h1>(123) 456-7890</h1>
+                        </div>
+                    </div>
+                </div>
+            </section>
         
         </>
      );
